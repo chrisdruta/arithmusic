9eadc1d959f276e683c528cded81a2c3d8caffe5
chris <cndruta@email.arizona.edu>
2018-12-25 02:27:43 -0700

    Added navbar

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,20 +1,36 @@
 import * as React from "react";
+
+import {Navbar, NavItem, Icon, CardTitle} from "react-materialize";
 import PlotlyChart from "react-plotlyjs-ts";
 
 export class App extends React.Component<{}, {}> {
     render() {
-        return (<PlotlyChart
-            data={[
-              {
-                x: [1, 2, 3],
-                y: [2, 6, 3],
-                type: 'scatter',
-                mode: 'lines',
-                marker: {color: 'red'},
-              },
-              {type: 'bar', x: [1, 2, 3], y: [2, 5, 3]},
-            ]}
-            layout={ {title: 'A Fancy Plot'} }
-          />)
+        return (
+          <div>
+            <Navbar brand={<span style={{paddingLeft: "5px"}}>TuneScript</span>} href="#" right>
+              <NavItem href='get-started.html'><Icon>play_arrow</Icon></NavItem>
+              <NavItem href='get-started.html'><Icon>stop</Icon></NavItem>
+              <NavItem href='get-started.html'><Icon>settings</Icon></NavItem>
+            </Navbar>
+            <PlotlyChart
+              data={[
+                {
+                  x: [1, 2, 3],
+                  y: [2, 6, 3],
+                  type: 'scatter',
+                  mode: 'lines',
+                  marker: {color: 'red'},
+                },
+                {
+                  type: 'line',
+                  x: [1, 2, 3],
+                  y: [2, 5, 3]},
+              ]}
+              config={{
+                displayModeBar: false
+              }}
+            />
+          </div>
+          )
     }
 }
