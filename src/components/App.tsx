919eb5428c8a9f17ee24757b1320e8ab69996c0d
chris <cndruta@email.arizona.edu>
2018-12-25 02:54:41 -0700

    Added function declarations to app class

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,21 +4,33 @@ import { Navbar, NavItem, Icon } from "react-materialize";
 import PlotlyChart from "react-plotlyjs-ts";
 
 export class App extends React.Component<{}, {}> {
+  play = (e: Event) => {
+    // Go through each tab and generate data then play
+  };
+
+  stop = (e: Event) => {
+    // Stop audio context
+  };
+
+  openSettings = (e: Event) => {
+    // Open dialogue for setting sample rate and possibly other settings
+  };
+
   render() {
     return (
       <div>
         <Navbar
-          brand={<span style={{ paddingLeft: "5px" }}>TuneScript</span>}
+          brand={<span style={{ paddingLeft: "10px" }}>TuneScript</span>}
           href="#"
           right
         >
-          <NavItem href="#">
+          <NavItem href="#" onClick={this.play}>
             <Icon>play_arrow</Icon>
           </NavItem>
-          <NavItem href="#">
+          <NavItem href="#" onClick={this.stop}>
             <Icon>stop</Icon>
           </NavItem>
-          <NavItem href="#">
+          <NavItem href="#" onClick={this.openSettings}>
             <Icon>settings</Icon>
           </NavItem>
         </Navbar>
