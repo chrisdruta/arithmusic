c1bdac55c577eb6722e6acf3938ac977b18d3e2b
chris <cndruta@email.arizona.edu>
2018-12-25 02:39:44 -0700

    Beutified document

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,36 +1,47 @@
 import * as React from "react";
 
-import {Navbar, NavItem, Icon, CardTitle} from "react-materialize";
+import { Navbar, NavItem, Icon } from "react-materialize";
 import PlotlyChart from "react-plotlyjs-ts";
 
 export class App extends React.Component<{}, {}> {
-    render() {
-        return (
-          <div>
-            <Navbar brand={<span style={{paddingLeft: "5px"}}>TuneScript</span>} href="#" right>
-              <NavItem href='get-started.html'><Icon>play_arrow</Icon></NavItem>
-              <NavItem href='get-started.html'><Icon>stop</Icon></NavItem>
-              <NavItem href='get-started.html'><Icon>settings</Icon></NavItem>
-            </Navbar>
-            <PlotlyChart
-              data={[
-                {
-                  x: [1, 2, 3],
-                  y: [2, 6, 3],
-                  type: 'scatter',
-                  mode: 'lines',
-                  marker: {color: 'red'},
-                },
-                {
-                  type: 'line',
-                  x: [1, 2, 3],
-                  y: [2, 5, 3]},
-              ]}
-              config={{
-                displayModeBar: false
-              }}
-            />
-          </div>
-          )
-    }
+  render() {
+    return (
+      <div>
+        <Navbar
+          brand={<span style={{ paddingLeft: "5px" }}>TuneScript</span>}
+          href="#"
+          right
+        >
+          <NavItem href="#">
+            <Icon>play_arrow</Icon>
+          </NavItem>
+          <NavItem href="#">
+            <Icon>stop</Icon>
+          </NavItem>
+          <NavItem href="#">
+            <Icon>settings</Icon>
+          </NavItem>
+        </Navbar>
+        <PlotlyChart
+          data={[
+            {
+              x: [1, 2, 3],
+              y: [2, 6, 3],
+              type: "scatter",
+              mode: "lines",
+              marker: { color: "red" }
+            },
+            {
+              type: "line",
+              x: [1, 2, 3],
+              y: [2, 5, 3]
+            }
+          ]}
+          config={{
+            displayModeBar: false
+          }}
+        />
+      </div>
+    );
+  }
 }
