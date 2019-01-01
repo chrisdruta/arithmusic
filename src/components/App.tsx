2ac28155a06329505c526052445e67ddb9f22044
chris <cndruta@email.arizona.edu>
2019-01-01 14:54:35 -0700

    Implented settings modal

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,41 +1,63 @@
 import * as React from "react";
-
-import { Navbar, NavItem, Icon } from "react-materialize";
 import PlotlyChart from "react-plotlyjs-ts";
 
-import {Tabs, TabList, Tab, DragTabList, DragTab, PanelList, Panel, ExtraButton} from 'react-tabtab';
+import { Navbar, NavItem, Icon, Modal, Row, Input } from "react-materialize";
 
-export class App extends React.Component<{}, {}> {
-  play = (e: Event) => {
-    // Go through each tab and generate data then play
-  };
+import {
+  Tabs,
+  DragTabList,
+  DragTab,
+  PanelList,
+  Panel,
+} from "react-tabtab";
 
-  stop = (e: Event) => {
-    // Stop audio context
-  };
+const initialState = {
+  isModalOpen: false,
+  fs: 44100
+};
 
-  openSettings = (e: Event) => {
-    // Open dialogue for setting sample rate and possibly other settings
-  };
+type State = Readonly<typeof initialState>;
+
+export class App extends React.Component<object, State> {
+
+  readonly state: State = initialState;
+
+  shouldComponentUpdate(nextProps: object, nextState: State) {
+    if (this.state.isModalOpen == nextState.isModalOpen && !nextState.isModalOpen)
+      return false;
+    return true;
+  }
 
   render() {
     return (
       <div>
         <Navbar
           brand={<span style={{ paddingLeft: "10px" }}>TuneScript</span>}
-          href="#"
+          href=""
           right
         >
-          <NavItem href="#" onClick={this.play}>
+          <NavItem onClick={this.handlePlay}>
             <Icon>play_arrow</Icon>
           </NavItem>
-          <NavItem href="#" onClick={this.stop}>
+          <NavItem onClick={this.handleStop}>
             <Icon>stop</Icon>
           </NavItem>
-          <NavItem href="#" onClick={this.openSettings}>
+          <NavItem onClick={this.handleOpenSettings}>
             <Icon>settings</Icon>
           </NavItem>
         </Navbar>
+        <Modal
+          open={this.state.isModalOpen}
+          header='Settings'
+          fixedFooter
+          modalOptions={{
+            complete: this.handleCloseSettings
+          }}
+        >
+          <Row>
+            <Input label="Sampling Frequency (Hz)" s={6} defaultValue={this.state.fs} />
+          </Row>
+        </Modal>
         <PlotlyChart
           data={[
             {
@@ -55,10 +77,36 @@ export class App extends React.Component<{}, {}> {
             displayModeBar: false
           }}
         />
-        <Tabs  >
-          
+        <Tabs>
+          <DragTabList>
+            <DragTab>Segment 1</DragTab>
+            <DragTab>Segment 2</DragTab>
+            <DragTab>Segment 3</DragTab>
+          </DragTabList>
+          <PanelList>
+            <Panel>Segment 1 Form</Panel>
+            <Panel>Segment 2 Form</Panel>
+            <Panel>Segment 3 Form</Panel>
+          </PanelList>
         </Tabs>
       </div>
     );
   }
+
+  private handlePlay = () => this.setState(playSegments);
+  private handleStop = () => this.setState(stopAudio);
+  private handleOpenSettings = () => this.setState(openSettings);
+  private handleCloseSettings = () => this.setState(closeSettings);
 }
+
+const playSegments = (prevState: State) => ({});
+
+const stopAudio = (prevState: State) => ({});
+
+const openSettings = (prevState: State) => ({
+  isModalOpen: true
+});
+
+const closeSettings = (prevState: State) => ({
+  isModalOpen: false
+});
