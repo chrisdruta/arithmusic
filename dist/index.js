8205bb437ccaa2bceca8466ca20a0886b4d8daf1
chris <cndruta@email.arizona.edu>
2019-01-01 22:04:53 -0700

    Removed dist to reduce repo size

--- dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const React = require("react");
-const ReactDOM = require("react-dom");
-const App_1 = require("./components/App");
-const LikeButton_1 = require("./components/LikeButton");
-ReactDOM.render(React.createElement(App_1.App, null), document.getElementById("app"));
-ReactDOM.render(React.createElement(LikeButton_1.LikeButton, null), document.getElementById("example-like"));
-//# sourceMappingURL=index.js.map
