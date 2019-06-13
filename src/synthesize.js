e0eb32ffd440644c67629e0c787c7110e7a712c6
Chris Druta <cndruta@email.arizona.edu>
2019-06-13 16:16:41 -0700

    Upated synthesization method (now works!)

--- src/synthesize.js
+++ src/synthesize.js
@@ -13,11 +13,11 @@ const SampleTrackGraph = (segments, multiplier) => {
       offsetMs += offsetLength;
     }
 
-    const sampleInput = range(0, lengthMs/1000, 1/1000);
+    const sampleInput = range(0, lengthMs / 1000, 1 / 1000);
     const func = (!segment.expression.error ? simplify(parse(segment.expression.value)) : parse("0")).compile();
     const sampleOutput = map(sampleInput, (x) => func.evaluate({ x: multiplier * x }));
-    const shiftedSampleInput = map(sampleInput, (x) => (x + offsetMs/1000));
-    
+    const shiftedSampleInput = map(sampleInput, (x) => (x + offsetMs / 1000));
+
     data.push({
       mode: "lines",
       x: shiftedSampleInput,
@@ -42,29 +42,31 @@ const SynthesizeComposition = (timelines, masterVolume, multiplier, fs, aliasing
 
   // Synthesize
   const bufferSize = maxLengthMs / 1000 * fs;
-  const rawBuffer = new Float32Array(bufferSize);
+  const rawBuffer = new Float32Array(bufferSize + 1);
 
   timelines.forEach((timeline, timelineIndex) => {
-    let bufferIndex = 0;
+    let bufferIndex = 1;
+    const phase = new Float32Array(bufferSize + 1);
 
     timeline.segments.forEach((segment, segmentIndex) => {
-      console.log(trackLengthsMs[timelineIndex] / 1000 * fs)
-      const sampleInput = range(0, trackLengthsMs[timelineIndex] / 1000 * fs);
       const func = (!segment.expression.error ? simplify(parse(segment.expression.value)) : parse("0")).compile();
-      const sampleOutput = aliasing ? map(sampleInput, (x) => func.evaluate({ x: multiplier * x }))
-                            : map(sampleInput, (x) => func.evaluate({ x: multiplier * x < (fs/2) ? multiplier * x : 0 }));
-      
-      let volumeMultiplier = masterVolume * (!timeline.segments[segmentIndex].volume.error ? timeline.segments[segmentIndex].volume.value : 0) / 10000;
-      sampleOutput.forEach((tone) => {
-        rawBuffer[bufferIndex] += volumeMultiplier * Math.cos(2 * Math.PI * tone * bufferIndex / fs);
-        bufferIndex++;
-      });
+      for (let i = 0; i <= segment.length.value / 1000; i += 1/fs) {
+        let tone = func.evaluate({x: i});
 
+        phase[bufferIndex] = phase[bufferIndex - 1] + 2 * Math.PI * tone/fs;
+        if (phase[bufferIndex] > 2 * Math.PI)
+          phase[bufferIndex] -= 2 * Math.PI;
+        else if (phase[bufferIndex] < -2 * Math.PI)
+          phase[bufferIndex] += 2 * Math.PI;
+
+        rawBuffer[bufferIndex] += 0.1 * Math.sin(phase[bufferIndex]);
+        bufferIndex++;
+      }
     });
-    console.log(`Buffer index: ${bufferIndex}`)
   });
-  console.log(`Buffer size: ${bufferSize}`)
   
+  // TODO: Exponentially decress gain near end
+
   return rawBuffer;
 
 };
@@ -73,7 +75,7 @@ const SynthesizeComposition = (timelines, masterVolume, multiplier, fs, aliasing
 //   timelines.forEach((timeline) => {
 //     let bufferIndex = 0;
 //     const sampledSegments = SampleTrack(timeline.segments, multiplier, fs);
-    
+
 //     sampledSegments.forEach((sample, index) => {
 //       let volumeMultiplier = masterVolume * (!timeline.segments[index].volume.error ? timeline.segments[index].volume.value : 0) / 100 / 100;
 //       sample.y.forEach((tone) => {
@@ -85,7 +87,7 @@ const SynthesizeComposition = (timelines, masterVolume, multiplier, fs, aliasing
 //       });
 //     });
 //   });
-  
+
 //   return rawBuffer;
 
 // };
