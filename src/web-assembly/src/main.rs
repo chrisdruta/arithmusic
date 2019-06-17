1795388bea32e5cc099887b07deb49680ef0dca1
Chris Druta <cndruta@email.arizona.edu>
2019-06-17 16:55:09 -0700

    Added rust source code for wasm

---
+++ src/web-assembly/src/main.rs
@@ -0,0 +1,38 @@
+use std::fs;
+extern crate serde;
+extern crate serde_json;
+use serde::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+struct TimelineOptions {
+    title: String,
+    wave_type: String,
+    mute: bool
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Segment {
+    title: String,
+    expression: String,
+    length: u16,
+    volume: i8
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Timeline {
+    options: TimelineOptions,
+    segments: Vec<Segment>
+}
+
+fn main() {
+    // The statements here will be executed when the compiled binary is called
+
+    // Print text to the console
+    println!("Hello World!");
+
+    let contents = fs::read_to_string("linear.json").unwrap();
+
+    let json: Vec<Timeline> = serde_json::from_str(&contents).expect("JSON was not well-formatted");
+
+    println!("json: {:?}", json[0].segments[0].expression);
+}
