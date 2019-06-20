ffea6d1f325a51586f93592a235137691787acae
Chris Druta <chris.aturd@gmail.com>
2019-06-20 13:50:14 -0700

    Update 6/20/19

---
+++ precache-manifest.c7edff2e976be79268f5b753f53863b4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "715bc7f19804138d79a8",
+    "url": "/f91c20199475b40e32ac.module.wasm"
+  },
+  {
+    "revision": "f3b775753d8d6f6b99d921e3fc946d43",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c462481a9d095542f828",
+    "url": "/static/css/main.ac0e6078.chunk.css"
+  },
+  {
+    "revision": "fc884facd39ba7946611",
+    "url": "/static/js/2.7e83e3c7.chunk.js"
+  },
+  {
+    "revision": "715bc7f19804138d79a8",
+    "url": "/static/js/3.74e6e6a3.chunk.js"
+  },
+  {
+    "revision": "c462481a9d095542f828",
+    "url": "/static/js/main.66619538.chunk.js"
+  },
+  {
+    "revision": "1cbda6881f392cccec0e",
+    "url": "/static/js/runtime~main.8186280b.js"
+  }
+]);
